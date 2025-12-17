f281e0b47d87a5b0a97b20a007509f402a4e77fe
Ericsson <manikeeric@gmail.com>
2025-12-17 08:12:54 +0000

    mail fixed

--- utils/send_email.py
+++ utils/send_email.py
@@ -1,14 +1,15 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
 from pydantic import BaseModel, EmailStr
 from typing import List, Optional
+import os
 
 
 conf = ConnectionConfig(
-    MAIL_USERNAME="manikeeric@gmail.com",
-    MAIL_PASSWORD="heoj tsdg rdkw xifv",
-    MAIL_FROM="Recyco <manikeeric@gmail.com>",
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
     MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
+    MAIL_SERVER= "smtp.gmail.com", 
     MAIL_STARTTLS=True,    
     MAIL_SSL_TLS=False,    
     USE_CREDENTIALS=True,
